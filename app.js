13cd8dcbe5bf423a492c3e3992a4762ee01fd05e
I Gede Yoga Asta Pratama <yoga.asta.pra@gmail.com>
2025-08-05 23:42:51 +0800

    add public folder, modify app.js

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 require('dotenv').config();
 
+const cors = require('cors');
+app.use(cors()); // penting di atas routes
+
+// ✅ Tambahkan ini untuk serve file gambar statis dari folder public
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Import routes
 const coffeeRoutes = require('./Routes/coffeeRoutes');
 const foodRoutes = require('./Routes/foodRoutes');
 const nonCoffeeRoutes = require('./Routes/noncoffeeRoutes');
@@ -9,11 +17,13 @@ const merchandiseRoutes = require('./Routes/merchandiseRoutes');
 
 app.use(express.json());
 
+// Gunakan routes
 app.use('/api', coffeeRoutes);
 app.use('/api', foodRoutes);
 app.use('/api', nonCoffeeRoutes);
 app.use('/api', merchandiseRoutes);
 
+// Jalankan server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
